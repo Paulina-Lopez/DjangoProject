8b895cc0931bb80a438f2bf6f7efbcc96d0b827b
Paulina-Lopez <lopezpaulina22@gmail.com>
2022-09-06 10:26:23 -0500

    Formulario de busquedas

--- movie/views.py
+++ movie/views.py
@@ -7,11 +7,7 @@ from .models import Movie
 
 def home(request):
     searchTerm = request.GET.get('searchMovie')
-    if searchTerm:
-        movies = Movie.objects.filter(title__icontains=searchTerm)
-    else:
-        movies = Movie.objects.all()
-    return render(request, 'home.html', {'searchTerm':searchTerm, 'movies': movies})
+    return render(request, 'home.html', {'searchTerm':searchTerm})
 
 def about(request):
     return render(request,'about.html')
